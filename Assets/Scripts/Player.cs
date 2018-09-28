c762c2cc60548b1fa86eb46e49885227ab3a5011
Spdoriano101 <43067378+Spdoriano101@users.noreply.github.com>
2018-09-28 16:22:22 +0100

    Character moves with controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    // Desinger variables
+
+    public float speed = 10;
+    public Rigidbody2D physicsBody;
+    public string horizontalAxis = "Horizontal";
+    public string verticalAxis = "Vertical";
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // get axis input from Unity
+        float leftRight = Input.GetAxis(horizontalAxis);
+        float upDown = Input.GetAxis(verticalAxis);
+
+        // Create direction vector from axis input
+        Vector2 direction = new Vector2(leftRight, upDown);
+
+        // Make direction vector length 1
+           direction = direction.normalized;
+
+        // Calculate velocity
+
+        Vector2 velocity = direction * speed;
+
+        // Give the velocty to the rigid body
+
+
+        physicsBody.velocity = velocity;
+	}
+}
